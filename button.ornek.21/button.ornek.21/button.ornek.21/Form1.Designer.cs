ae148616be0624f6e20b9a09b52bd8205b15c9b0
busramerve37 <145451633+busramerve37@users.noreply.github.com>
2023-10-09 15:08:52 +0300

    Add files via upload

---
+++ button.ornek.21/button.ornek.21/button.ornek.21/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace button.ornek._21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOrtalama = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.txtOrtalama = new System.Windows.Forms.TextBox();
+            this.btnKontrolEt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOrtalama
+            // 
+            this.lblOrtalama.AutoSize = true;
+            this.lblOrtalama.Location = new System.Drawing.Point(30, 32);
+            this.lblOrtalama.Name = "lblOrtalama";
+            this.lblOrtalama.Size = new System.Drawing.Size(49, 13);
+            this.lblOrtalama.TabIndex = 0;
+            this.lblOrtalama.Text = "Ortalama";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(30, 101);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(38, 13);
+            this.lblSonuc.TabIndex = 1;
+            this.lblSonuc.Text = "Sonuc";
+            // 
+            // txtOrtalama
+            // 
+            this.txtOrtalama.Location = new System.Drawing.Point(96, 24);
+            this.txtOrtalama.Name = "txtOrtalama";
+            this.txtOrtalama.Size = new System.Drawing.Size(100, 20);
+            this.txtOrtalama.TabIndex = 2;
+            // 
+            // btnKontrolEt
+            // 
+            this.btnKontrolEt.Location = new System.Drawing.Point(337, 68);
+            this.btnKontrolEt.Name = "btnKontrolEt";
+            this.btnKontrolEt.Size = new System.Drawing.Size(75, 78);
+            this.btnKontrolEt.TabIndex = 3;
+            this.btnKontrolEt.Text = "Kontrol Et";
+            this.btnKontrolEt.UseVisualStyleBackColor = true;
+            this.btnKontrolEt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 342);
+            this.Controls.Add(this.btnKontrolEt);
+            this.Controls.Add(this.txtOrtalama);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.lblOrtalama);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOrtalama;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.TextBox txtOrtalama;
+        private System.Windows.Forms.Button btnKontrolEt;
+    }
+}
+
---
+++ button.ornek.21/button.ornek.21/button.ornek.21/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button.ornek._21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float ortalama;
+            ortalama=Convert.ToSingle(txtOrtalama.Text);
+            if (ortalama<0)
+            {
+                lblSonuc.Text = "Negatif Değer Giremezsin";
+            }
+            else if (ortalama<50)
+            {
+                lblSonuc.Text = "Kaldınız";
+
+            }
+            else if (ortalama<70)
+            {
+                lblSonuc.Text = "Geçtin";
+            }
+            else if (ortalama<85)
+            {
+                lblSonuc.Text = "Teşekkür Belgesi Aldın";
+
+            }
+            else if (ortalama<=100)
+            {
+                lblSonuc.Text = "Takdir Belgesi Aldın ";
+            }
+            else
+            {
+                lblSonuc.Text = "Düzgün Not Yaz";
+            }
+        }
+    }
+}
